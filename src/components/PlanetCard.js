69e01095581529c2234e31dedb58b0e1d08a0b2c
Izabella Cristina <oliveira_cavalcante_@hotmail.com>
2022-06-22 13:52:33 -0300

    Possisionamento dos itens no CSS

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -5,9 +5,9 @@ class PlanetCard extends React.Component {
   render() {
     const { planetName, planetImage } = this.props;
     return (
-      <div data-testid="planet-card">
+      <div data-testid="planet-card" className={ planetName }>
         <p data-testid="planet-name">{planetName}</p>
-        <img src={ planetImage } alt={ `Planeta ${planetName}` } className="planetinhas"/>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` }/>
       </div>
     );
   }
