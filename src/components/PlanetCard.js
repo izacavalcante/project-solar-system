aac83bf9183e39241ccc3f550159839b659b15c3
Izabella Cristina <oliveira_cavalcante_@hotmail.com>
2022-06-23 23:07:40 -0300

    Desenvolvendo o CSS

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -7,7 +7,7 @@ class PlanetCard extends React.Component {
     return (
       <div data-testid="planet-card" className={ planetName }>
         <p data-testid="planet-name" className="namePlanet">{planetName}</p>
-        <img src={ planetImage } alt={ `Planeta ${planetName}` }/>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } className="planeta" />
       </div>
     );
   }
